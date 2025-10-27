a6ee2f0a185d93cbcca13d76b217c358ba43644d
raphpay <raphpay.dev@gmail.com>
2025-10-27 15:40:52 +0400

    Correct and test email validation method #16

--- src/shared/utils/validation.ts
+++ src/shared/utils/validation.ts
@@ -4,7 +4,6 @@
  * @returns A boolean indicating whether the email address is valid or not.
  */
 export function isEmailValid(input: string): boolean {
-  const regex: RegExp = /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}/;
-  const isValid: boolean = regex.test(input);
-  return isValid;
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+  return regex.test(input.trim());
 }
