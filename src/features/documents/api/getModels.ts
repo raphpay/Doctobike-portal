4b0eca2a18f46fda4fce284191bf7ead3a9d3285
raphpay <raphpay.dev@gmail.com>
2025-11-04 15:01:55 +0400

    Fix page size #22

--- src/features/documents/api/getModels.ts
+++ src/features/documents/api/getModels.ts
@@ -6,7 +6,7 @@ export type ModelsResponse = {
 import { supabase } from "@/lib/supabase";
 
 export async function getModels(brand: string, page: number) {
-  const PAGE_SIZE = 3ﬂ0;
+  const PAGE_SIZE = 40;
   const start = (page - 1) * PAGE_SIZE; // Page 1 = start 0
   const end = start + PAGE_SIZE - 1;
 
